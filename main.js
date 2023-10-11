b592075ed60c5102d1bedb9174a1c721f79d24c4
Magnus Roland Raadla <magnus.roland.raadla@gmail.com>
2023-10-11 16:15:12 +0300

    TUNNITÖÖ

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
+import boxen from 'boxen';
 import chalk from 'chalk';
 
-function write(text) {
-    process.stdout.write(text);
-}
-
-console.log(chalk.bgGreen.magenta('Mystic is a great class'));
-console.log(chalk.italic('Mystic is a great class'));
-console.log(chalk.bgGreen.magenta('Mystic is a great class'));
-console.log(chalk.strikethrough('Mystic is a great class'));
-
 console.log(boxen('box', {borderStyle: 'round'}))
+console.log(boxen(chalk.blue.bold('POOOWEEEEERRRRR'), {borderColor: 'blue' ,borderStyle: 'round', backgroundColor: 'cyan', width: 12, height: 12, title: 'Jeremy Fragrance'}))
+console.log(boxen(chalk.red.bgCyan.italic('Morningstar'), {borderColor: 'red', borderStyle: 'classic', padding: 2, margin: 2, height: 18, width: 100, textAlignment: 'right', backgroundColor: 'yellow'}))
+console.log(boxen(chalk.black.bold('Ippo'), {borderStyle: 'arrow', margin: 1, padding: 4, backgroundColor: 'blue'}))
+console.log(boxen(chalk.bgRed('boxxx'), {borderStyle: 'doubleSingle', margin: 10, padding: 0, width: 80, backgroundColor: 'green'}))
+console.log(boxen('boxer', {padding: 15, margin: 5, borderStyle: 'bold', title: 'boxing', titleAlignment: 'left'}));
