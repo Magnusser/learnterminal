ab7f202583c266b4e8312341b750eeee73b5d078
Magnus Roland Raadla <magnus.roland.raadla@gmail.com>
2023-10-16 09:19:40 +0300

    weird ass

--- main.js
+++ main.js
@@ -1,9 +1,45 @@
 import boxen from 'boxen';
 import chalk from 'chalk';
+import readline from 'readline';
+function write(text) {
+    process.stdout.write(text);
+}
 
-console.log(boxen('box', {borderStyle: 'round'}))
-console.log(boxen(chalk.blue.bold('POOOWEEEEERRRRR'), {borderColor: 'blue' ,borderStyle: 'round', backgroundColor: 'cyan', width: 12, height: 12, title: 'Jeremy Fragrance'}))
-console.log(boxen(chalk.red.bgCyan.italic('Morningstar'), {borderColor: 'red', borderStyle: 'classic', padding: 2, margin: 2, height: 18, width: 100, textAlignment: 'right', backgroundColor: 'yellow'}))
-console.log(boxen(chalk.black.bold('Ippo'), {borderStyle: 'arrow', margin: 1, padding: 4, backgroundColor: 'blue'}))
-console.log(boxen(chalk.bgRed('boxxx'), {borderStyle: 'doubleSingle', margin: 10, padding: 0, width: 80, backgroundColor: 'green'}))
-console.log(boxen('boxer', {padding: 15, margin: 5, borderStyle: 'bold', title: 'boxing', titleAlignment: 'left'}));
+readline.emitKeypressEvents(process.stdin);
+
+if(process.stdin.isTTY){
+    process.stdin.setRawMode(true);
+}
+
+
+
+write('@');
+
+process.stdin.on('keypress', (chunk, key)=>{
+    if(key && key.name === 'c' && key.ctrl === true){
+        process.exit();
+    }
+    if(key && key.name === 'd'){
+        write('\x1B[1D');
+        write(' @');
+    }
+    if(key && key.name === 's'){
+        write('\x1B[1D');
+        write(' ');
+        write('\x1B[1D');
+        write('\x1B[1B');
+        write('@');
+    }
+    if(key && key.name === 'a'){
+        write('\x1B[2C');
+        write('@ ');
+        write('\x1B[1D');
+    }
+    if(key && key.name === 's'){
+        write('\x1B[1D');
+        write(' ');
+        write('\x1B[1D');
+        write('\x1B[1A');
+        write('@');
+    }
+});
